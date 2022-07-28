46a8bedd775cc5bfdfc22acb10b1587269d63d95
Josep Mulet Pol <pep.mulet@gmail.com>
2022-07-28 14:52:55 +0200

    initial commit

---
+++ src/main.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+import { createApp } from 'vue'
+import App from './App.vue';
+
+import PrimeVue from 'primevue/config';
+import InputText from 'primevue/inputtext';
+import SelectButton from 'primevue/selectbutton';
+import Fieldset from 'primevue/fieldset';
+import Slider from 'primevue/slider';
+import Card from 'primevue/card';
+import Badge from 'primevue/badge';
+import Panel from 'primevue/panel';
+import InputNumber from 'primevue/inputnumber';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
+
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css'; 
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+app.use(ToastService);
+
+app.component('InputText', InputText);
+app.component('SelectButton', SelectButton);
+app.component('Panel', Panel);
+app.component('Fieldset', Fieldset);
+app.component('Slider', Slider);
+app.component('Toast', Toast);
+app.component('Card', Card);
+app.component('InputNumber', InputNumber);
+app.component('Badge', Badge);
+
+app.mount('#app')
